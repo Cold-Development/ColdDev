9d612630f1cbeceb9136c6a7ae4cb2b7943e7101
padrewin <97427230+padrewin@users.noreply.github.com>
2024-09-10 14:59:22 +0300

    upload

---
+++ src/main/java/dev/padrewin/coldplugin/ColdPlugin.java
@@ -0,0 +1,17 @@
+package dev.padrewin.coldplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ColdPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
